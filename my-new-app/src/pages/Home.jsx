a7fc1337f25a38e606ea3387ed54e24e7705aa5e
KittikonDonde <kittikon1659@gmail.com>
2024-01-26 15:18:38 +0700

    adwww

--- my-new-app/src/pages/Home.jsx
+++ my-new-app/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Sidebar from '../components/Sidebar';
+import Navbar from '../components/Navbar';
 
 function Home() {
     return (
-        <div>
-            <h2>Welcome to the Home Page!</h2>
-            <p>This is the home page content.</p>
+        <div class="wrapper">
+        <Sidebar />
+        <Navbar />
         </div>
     );
 }
