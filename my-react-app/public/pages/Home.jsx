a8cb842f9d2ab66bdb629d9531a06f11e6dd0a78
KittikonDonde <kittikon1659@gmail.com>
2024-01-26 10:11:57 +0700

    +

---
+++ my-react-app/public/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NewPage() {
+  return (
+    <div>
+      <h1>New Page</h1>
+      <p>This is a new page in your React app!</p>
+    </div>
+  );
+}
+
+export default NewPage;
