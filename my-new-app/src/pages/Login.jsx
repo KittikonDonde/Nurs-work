edd3716d259be52fc6bf480b552b4b14fdb8f69a
KittikonDonde <kittikon1659@gmail.com>
2024-02-02 10:19:26 +0700

    adnew

--- my-new-app/src/pages/Login.jsx
+++ my-new-app/src/pages/Login.jsx
@@ -11,7 +11,7 @@ function Login() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post('http://localhost:3001/login', { username, password });
+            const response = await axios.post('http://172.16.61.7:3001/login', { username, password });
             console.log(response.data);
             setError('');
             navigate('/home')
